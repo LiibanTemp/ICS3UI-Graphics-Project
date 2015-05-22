953a4d401c43f09866b4ac84ddf5d9758d32c69e
gulat5020 <gulat5020@FHC-IC0037271.wrdsb.ca>
2015-05-22 13:03:30 -0400

    Pan

---
+++ src/you/da/man/PanMain.java
@@ -0,0 +1,20 @@
+package you.da.man;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author gulat5020
+ */
+public class PanMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
