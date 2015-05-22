e170302a70bd7024db21b741db376c3a50883584
nurl3755 <nurl3755@FHC-IC0037270.wrdsb.ca>
2015-05-22 12:49:41 -0400

    YouDaMan

---
+++ src/you/da/man/YouDaMan.java
@@ -0,0 +1,12 @@
+//Liiban Nur
+//Timothy Gulabsing
+//Prabh Sukhija
+package you.da.man;
+
+public class YouDaMan {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        FraMain fraMain = new FraMain();
+    }
+}
