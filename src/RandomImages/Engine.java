a76a6f20cc14084cb0a1ab80e2a973ae2e6b5699
sukhp3632 <sukhp3632@FHC-IC0037271.wrdsb.ca>
2015-05-26 14:22:06 -0400

    ITEMS LOLOLOL

---
+++ src/RandomImages/Engine.java
@@ -0,0 +1,18 @@
+
+package RandomImages;
+
+/**
+ *
+ * @author sukhp3632
+ */
+public class Engine {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        FraMain fraMain = new FraMain();
+        Sprites pepsi = new Sprites();
+        //pepsi.loadSprite();
+    }
+}
